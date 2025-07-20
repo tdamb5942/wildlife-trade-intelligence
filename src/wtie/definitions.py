7220fca545c4339b7c89708f79587124d413a72b
tdamb5942 <tdamb5942@gmail.com>
2025-07-20 14:43:27 +0100

    add dagster assets

--- src/wtie/definitions.py
+++ src/wtie/definitions.py
@@ -1,6 +1,24 @@
 from dagster import Definitions
-from wtie.defs import hello_wtie
+from wtie.defs import (
+    raw_cites_data,
+    cites_data,
+    synthetic_data,
+    combined_trade_data,
+    top_species_by_volume,
+    top_trade_routes,
+)
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
 
 defs = Definitions(
-    assets=[hello_wtie],
+    assets=[
+        raw_cites_data,
+        cites_data,
+        synthetic_data,
+        combined_trade_data,
+        top_species_by_volume,
+        top_trade_routes,
+    ],
 )
